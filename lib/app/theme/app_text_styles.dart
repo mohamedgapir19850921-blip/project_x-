d38586ee1599acc010c3a89be3ef4c401fdad8b2
mohamedgapir19850921-blip <mohamedgapir19850921@gmail.com>
2026-09-16 05:37:53 +0300

    Refactor text style methods to use braces

--- lib/app/theme/app_text_styles.dart
+++ lib/app/theme/app_text_styles.dart
@@ -3,33 +3,43 @@ import 'package:flutter/material.dart';
 class AppTextStyles {
   AppTextStyles._();
 
-  static TextStyle headingLarge(Color color) => TextStyle(
-        fontSize: 28,
-        fontWeight: FontWeight.bold,
-        color: color,
-      );
+  static TextStyle headingLarge(Color color) {
+    return TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.bold,
+      color: color,
+    );
+  }
 
-  static TextStyle headingMedium(Color color) => TextStyle(
-        fontSize: 22,
-        fontWeight: FontWeight.w700,
-        color: color,
-      );
+  static TextStyle headingMedium(Color color) {
+    return TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
 
-  static TextStyle title(Color color) => TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.w600,
-        color: color,
-      );
+  static TextStyle title(Color color) {
+    return TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
 
-  static TextStyle body(Color color) => TextStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w400,
-        color: color,
-      );
+  static TextStyle body(Color color) {
+    return TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
 
-  static TextStyle label(Color color) => TextStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.w500,
-        color: color,
-      );
+  static TextStyle label(Color color) {
+    return TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
 }
