2119570b87bd134c501fda828aebbe4fa87e2204
mohamedgapir19850921-blip <mohamedgapir19850921@gmail.com>
2026-09-16 04:56:42 +0300

    Implement ThemeProvider for theme mode management

---
+++ lib/features/theme_mode/providers/theme_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+
+  void setThemeMode(ThemeMode mode) {
+    _themeMode = mode;
+    notifyListeners();
+  }
+
+  void setLightMode() {
+    _themeMode = ThemeMode.light;
+    notifyListeners();
+  }
+
+  void setDarkMode() {
+    _themeMode = ThemeMode.dark;
+    notifyListeners();
+  }
+
+  void setSystemMode() {
+    _themeMode = ThemeMode.system;
+    notifyListeners();
+  }
+}
