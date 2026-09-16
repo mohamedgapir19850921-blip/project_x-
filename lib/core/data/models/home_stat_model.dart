52b09caf04d7b4cd5a9a5f72a4d6df5c88cffb0e
mohamedgapir19850921-blip <mohamedgapir19850921@gmail.com>
2026-09-16 16:22:07 +0300

    Add HomeStatModel class for home statistics

---
+++ lib/core/data/models/home_stat_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeStatModel {
+  final String title;
+  final String value;
+  final IconData icon;
+  final Color color;
+
+  const HomeStatModel({
+    required this.title,
+    required this.value,
+    required this.icon,
+    required this.color,
+  });
+}
