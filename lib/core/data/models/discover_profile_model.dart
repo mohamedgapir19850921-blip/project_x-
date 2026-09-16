9049a846fc86353cecd3def054289c8020fa8074
mohamedgapir19850921-blip <mohamedgapir19850921@gmail.com>
2026-09-16 16:14:33 +0300

    Create DiscoverProfileModel class with properties

---
+++ lib/core/data/models/discover_profile_model.dart
@@ -0,0 +1,17 @@
+class DiscoverProfileModel {
+  final String name;
+  final int age;
+  final String city;
+  final String match;
+  final List<String> tags;
+  final List<int> gradientColors;
+
+  const DiscoverProfileModel({
+    required this.name,
+    required this.age,
+    required this.city,
+    required this.match,
+    required this.tags,
+    required this.gradientColors,
+  });
+}
